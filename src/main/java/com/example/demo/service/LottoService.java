826d9d2e85601340df39e1faa990950a1ea321ad
zooey <yu.ting375@gmail.com>
2025-06-04 11:45:18 +0800

    20250604_1145

--- src/main/java/com/example/demo/service/LottoService.java
+++ src/main/java/com/example/demo/service/LottoService.java
@@ -3,16 +3,17 @@ package com.example.demo.service;
 import java.util.Random;
 
 import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LottoService {
 
-	/* 可以得到lotto電腦選號
+	/* 可以得到樂透電腦選號
 	* @param amount 購買數量
 	*/ 
-	@Tool(name = "lotto", description = "可以得到lotto電腦選號")
-	public String lotto593(Integer amount) {
+	@Tool(name = "lotto", description = "可以得到樂透電腦選號")
+	public String lotto(@ToolParam(description = "樂透電腦選號購買的數量") Integer amount) {
 		Random random = new Random();
 		return "LottoService 產生樂透號碼:" + random.nextInt(10); // 0~9，回傳給字串
 	}
@@ -20,7 +21,8 @@ public class LottoService {
 	/* 樂透結帳
 	* @param amount 購買數量
 	*/
-	public String checkout(Integer amount) {
+	@Tool(name = "checkout", description = "樂透結帳/付款")
+	public String checkout(@ToolParam(description = "要結帳的數量") Integer amount) {
 		return "LottoService 樂透結帳:$" + (amount *50);
 		
 	}
